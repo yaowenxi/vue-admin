58ab3b29c402509f2aeacdd70cd1eca9e2af67e1
yaowx001 <yaowx001@cmrh.com>
2018-06-14 10:52:14 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Home from './components/page/Home'
+
+export default {
+  name: 'App',
+  comments: {Home}
+}
+</script>
+
+<style>
+  .fa-icon {
+    height: 1em;
+    width: 1em;
+    bottom: -0.125em;
+    position: relative;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+
+Vue.config.productionTip = false
+
+Vue.use(iView)
+Vue.component('icon', Icon)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
